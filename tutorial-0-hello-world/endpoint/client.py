86c015f328f9992d2f2fce72056ca9799dea2484
Utkarsh Kashyap <107147005+ob-uk@users.noreply.github.com>
2025-08-21 09:31:52 -0700

    Update auth() function

--- tutorial-0-hello-world/endpoint/client.py
+++ tutorial-0-hello-world/endpoint/client.py
@@ -2,14 +2,17 @@
 import requests
 import os
 
-def get_auth_headers():
-    if os.environ.get('METAFLOW_SERVICE_AUTH_KEY'):
-        return {"x-api-key": os.environ.get('METAFLOW_SERVICE_AUTH_KEY')}
-    from outerbounds_app_client import OuterboundsAppClient
-    return OuterboundsAppClient().get_auth_headers()
+def auth():
+    from metaflow.metaflow_config_funcs import init_config
+    conf = init_config()
+    if conf:
+        headers = {'x-api-key': conf['METAFLOW_SERVICE_AUTH_KEY']}
+    else:
+        headers = json.loads(os.environ['METAFLOW_SERVICE_HEADERS'])
+    return headers
 
 
-headers = get_auth_headers()
+headers = auth()
 # TODO: Change to your own endpoint
 url = "https://api-c-egy05g.dev-yellow.outerbounds.xyz"
 print(requests.get(url, headers=headers).text)
